260497c4eda7ea4f6fd8f719fb9f55c5fcc44437
Rendyxsiip <101471496+Rendyxsiip@users.noreply.github.com>
2022-03-13 00:15:39 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,38 +1,35 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-// Website Api
 global.APIs = {
 	alfa: 'https://api-alphabot.herokuapp.com',
 }
 
-// Free apikey
 global.APIKeys = {
 	'https://api-alphabot.herokuapp.com': 'Alphabot',
 }
 
-// setting 
-global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
 
-// Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.autoread = false
+global.autorecording = true 
+global.autoketik = false
+global.available = false
+
+global.botname = "NeverSurender"
+global.ownername= "siip" 
+global.myweb ="https://api-alphabot.herokuapp.com/" 
+global.youtube = "https://youtube.com/c/ZeeoneOfc"
+global.github = "https://zeeone-ofc.github.io/"
+global.email = "zeeoneofc@gmail.com"
+global.region = "Indonesia"
+global.ownernomer = "+6281264393470"
+global.ownernomerr = "+6281264393470"
+global.thumbnail = "./image/lol.jpg"
+global.donasi = "./image/donasi.jpg"
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg"
+global.owner = ["6281264393470","6281263720971"]
+global.packname = 'NeverSurender'
+global.author = 'Di Buat Oleh siip'
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
